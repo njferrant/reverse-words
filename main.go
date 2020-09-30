847a0d1bc581c16942da1402f6466351bf77bcf4
Nick Ferrant <nferrant@sandbox3-linux.ad.nadlab.com>
2020-09-30 16:59:30 -0400

    Release updated to

--- main.go
+++ main.go
@@ -40,7 +40,7 @@ var (
 	)
 )
 
-var version = "v0.0.15"
+var version = ""
 
 // ReturnRelease returns the release configured by the user
 func ReturnRelease(w http.ResponseWriter, r *http.Request) {
